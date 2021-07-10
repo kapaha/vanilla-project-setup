7ed61366ea19a7f352f94acf1bf59cde9b5c72fd
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2021-07-10 17:38:37 +0100

    Remove dist

--- dist/main.53a4926e826ef7fdc4c2.bundle.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";document.querySelector(".test").addEventListener("click",(function(){console.log("Hello World")}))})();
-//# sourceMappingURL=main.53a4926e826ef7fdc4c2.bundle.js.map
